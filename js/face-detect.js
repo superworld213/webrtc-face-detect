147229d9c6762ccb4f447d0750306fa116b9f87d
BY <1157532150@qq.com>
2023-05-17 14:31:09 +0800

    更新

--- js/face-detect.js
+++ js/face-detect.js
@@ -1,39 +1,42 @@
 (function () {
-    var startFaceDetectBtn = document.querySelector('#startFaceDetect');
+  var startFaceDetectBtn = document.querySelector('#startFaceDetect');
 
-    // 画布控制器
-    var CanvasCtrl = {
-        canvas: null,
-        ctx: null,
-        score: null,
-        setCanvasSize: function () {
-            if (!this.canvas) return;
-            this.canvas.width = parseFloat(window.getComputedStyle(MediaCtrl.video).width);
-            this.canvas.height = parseFloat(window.getComputedStyle(MediaCtrl.video).height);
-        },
-        setCanvas: function () {
-            var canvas = document.createElement('canvas');
-            canvas.id = 'webrtcCanvas';
+  // 画布控制器
+  var CanvasCtrl = {
+    canvas: null,
+    ctx: null,
+    score: null,
+    setCanvasSize: function () {
+      if (!this.canvas) return;
+      this.canvas.width = parseFloat(window.getComputedStyle(MediaCtrl.video).width);
+      this.canvas.height = parseFloat(window.getComputedStyle(MediaCtrl.video).height);
+    },
+    setCanvas: function () {
+      var canvas = document.createElement('canvas');
+      canvas.id = 'webrtcCanvas';
 
-            var score = document.createElement('span');
-            score.id = 'faceScore';
+      var score = document.createElement('span');
+      score.id = 'faceScore';
 
-            MediaCtrl.videoWrapper.appendChild(canvas);
-            MediaCtrl.demoWrapper.appendChild(score);
+      MediaCtrl.videoWrapper.appendChild(canvas);
+      MediaCtrl.demoWrapper.appendChild(score);
 
-            this.canvas = document.querySelector('#webrtcCanvas');
-            this.ctx = this.canvas.getContext('2d');
-            this.score = document.querySelector('#faceScore');
-        },
-        removeCanvas: function () {
-            try {
-                MediaCtrl.videoWrapper.removeChild(this.canvas);
-                MediaCtrl.demoWrapper.removeChild(this.score);
-                this.canvas = this.score = null;
-            } catch (error) { }
-        },
-        screenshot:function(){
-          setTimeout(() => {
+      this.canvas = document.querySelector('#webrtcCanvas');
+      this.ctx = this.canvas.getContext('2d');
+      this.score = document.querySelector('#faceScore');
+    },
+    removeCanvas: function () {
+      try {
+        MediaCtrl.videoWrapper.removeChild(this.canvas);
+        MediaCtrl.demoWrapper.removeChild(this.score);
+        this.canvas = this.score = null;
+      } catch (error) { }
+    },
+    screenshot: function () {
+      return new Promise((reseolve, reject) => {
+
+        setTimeout(() => {
+          try {
             const video = document.querySelector('#localVideo');
             const canvas = document.getElementById('webrtcCanvasScreenshot');
             const canvasCtx = canvas.getContext('2d');
@@ -41,103 +44,110 @@
             const height = parseFloat(window.getComputedStyle(MediaCtrl.video).height);
             canvas.width = width;
             canvas.height = height;
-            canvasCtx.drawImage(video,0,0,width,height);
+            canvasCtx.drawImage(video, 0, 0, width, height);
             const MIME_TYPE = 'image/png'; // 保存文件类型
             const imgURL = canvas.toDataURL(MIME_TYPE);
             alert(imgURL);
-          }, 100);
-        },
-    };
+            reseolve(imgURL)
+          } catch(err) {
+            reject(err)
+          }
+        }, 500);
+      })
 
-    // 人脸识别控制器
-    var FaceDetectCtrl = {
-        ctracker: null,
-        ctrackerTimer: -1,
-        passScore: 60,
-        goodScore: 70,
-        throttle: true,
-        start: function () {
-            if (!CanvasCtrl.canvas) return;
+    },
+  };
 
-            if (!this.ctracker) {
-                this.ctracker = new clm.tracker();
-                this.ctracker.init();
-            } else this.stop();
+  // 人脸识别控制器
+  var FaceDetectCtrl = {
+    ctracker: null,
+    ctrackerTimer: -1,
+    passScore: 60,
+    goodScore: 70,
+    throttle: true,
+    start: function () {
+      if (!CanvasCtrl.canvas) return;
 
-            this.ctracker.start(MediaCtrl.video);
-            this.detect();
-        },
-        detect: function () {
-            this.ctrackerTimer = requestAnimationFrame(this.detect.bind(this));
+      if (!this.ctracker) {
+        this.ctracker = new clm.tracker();
+        this.ctracker.init();
+      } else this.stop();
 
-            CanvasCtrl.ctx.clearRect(0, 0, CanvasCtrl.canvas.width, CanvasCtrl.canvas.height);
-            
-            if (this.ctracker.getCurrentPosition()) {
-                var score = Math.round(this.ctracker.getScore() * 100);
-                FaceDetectCtrl.setFaceScore(score);
-                if (score >= this.passScore) {
-                    this.ctracker.draw(CanvasCtrl.canvas);
-                }
-            } else {
-                FaceDetectCtrl.setFaceScore(0);
-            }
-        },
-        setFaceScore: function (score) {
-            var _this = this;
-            if (!CanvasCtrl.score) return;
-            if (+CanvasCtrl.score.innerText === score) return;
-            if (!_this.throttle) return;
+      this.ctracker.start(MediaCtrl.video);
+      this.detect();
+    },
+    detect: function () {
+      this.ctrackerTimer = requestAnimationFrame(this.detect.bind(this));
 
-            _this.throttle = false;
+      CanvasCtrl.ctx.clearRect(0, 0, CanvasCtrl.canvas.width, CanvasCtrl.canvas.height);
 
-            setTimeout(function () {
-                if (CanvasCtrl.score) {
-                    // var color = score >= _this.goodScore ? 'green'
-                    //     : score >= _this.passScore ? 'yellow' : '';
-                    // CanvasCtrl.score.className = color;
-                    // CanvasCtrl.score.innerText = score;
-                    CanvasCtrl.score.className = 'green';
-                    CanvasCtrl.score.innerText = '检测通过'
-                    this.stop()
-                }
-                _this.throttle = true;
-            }, 500);
-        },
-        stop: function () {
-            if (!this.ctracker || !CanvasCtrl.canvas) return;
-            this.ctracker.stop();
-            this.ctracker.reset();
-            CanvasCtrl.ctx.clearRect(0, 0, CanvasCtrl.canvas.width, CanvasCtrl.canvas.height);
-            cancelAnimationFrame(this.ctrackerTimer);
+      if (this.ctracker.getCurrentPosition()) {
+        var score = Math.round(this.ctracker.getScore() * 100);
+        FaceDetectCtrl.setFaceScore(score);
+        if (score >= this.passScore) {
+          this.ctracker.draw(CanvasCtrl.canvas);
         }
-    };
+      } else {
+        FaceDetectCtrl.setFaceScore(0);
+      }
+    },
+    setFaceScore: function (score) {
+      var _this = this;
+      if (!CanvasCtrl.score) return;
+      if (+CanvasCtrl.score.innerText === score) return;
+      if (!_this.throttle) return;
 
-    startFaceDetectBtn.addEventListener('click', function () {
-        // 监听关闭按钮
-        MediaCtrl.listenClose(function () {
-            // 停止识别
-            FaceDetectCtrl.stop();
-            // 移除canvas
-            CanvasCtrl.removeCanvas();
-        });
-        // 监听video变化
-        MediaCtrl.listenResize(function () {
-            logger('setCanvasSize', '设置canvas宽高');
-            CanvasCtrl.setCanvasSize();
-            FaceDetectCtrl.start();
-        });
-        // 调用摄像头
-        MediaCtrl.start().then(function () {
-            // 设置canvas
-            CanvasCtrl.setCanvas();
-            CanvasCtrl.setCanvasSize();
-            // 开始识别人脸
-            FaceDetectCtrl.start();
-        }).catch(function (error) {
-            logger('getUserMedia', error);
+      _this.throttle = false;
 
-            FaceDetectCtrl.stop();
-            CanvasCtrl.removeCanvas();
-        })
+      setTimeout(function () {
+        if (CanvasCtrl.score > 50) {
+          // var color = score >= _this.goodScore ? 'green'
+          //     : score >= _this.passScore ? 'yellow' : '';
+          // CanvasCtrl.score.className = color;
+          // CanvasCtrl.score.innerText = score;
+          CanvasCtrl.score.className = 'green';
+          CanvasCtrl.score.innerText = '检测通过'
+          CanvasCtrl.screenshot()
+          this.stop()
+        }
+        _this.throttle = true;
+      }, 500);
+    },
+    stop: function () {
+      if (!this.ctracker || !CanvasCtrl.canvas) return;
+      this.ctracker.stop();
+      this.ctracker.reset();
+      CanvasCtrl.ctx.clearRect(0, 0, CanvasCtrl.canvas.width, CanvasCtrl.canvas.height);
+      cancelAnimationFrame(this.ctrackerTimer);
+    }
+  };
+
+  startFaceDetectBtn.addEventListener('click', function () {
+    // 监听关闭按钮
+    MediaCtrl.listenClose(function () {
+      // 停止识别
+      FaceDetectCtrl.stop();
+      // 移除canvas
+      CanvasCtrl.removeCanvas();
     });
+    // 监听video变化
+    MediaCtrl.listenResize(function () {
+      logger('setCanvasSize', '设置canvas宽高');
+      CanvasCtrl.setCanvasSize();
+      FaceDetectCtrl.start();
+    });
+    // 调用摄像头
+    MediaCtrl.start().then(function () {
+      // 设置canvas
+      CanvasCtrl.setCanvas();
+      CanvasCtrl.setCanvasSize();
+      // 开始识别人脸
+      FaceDetectCtrl.start();
+    }).catch(function (error) {
+      logger('getUserMedia', error);
+
+      FaceDetectCtrl.stop();
+      CanvasCtrl.removeCanvas();
+    })
+  });
 })();
